282e4278fd2a2ce824a64275559862e543992e9a
Barny <BarnyK@users.noreply.github.com>
2019-03-27 13:13:11 +0100

    removed node from private part of sequence

--- testtemplates/templates.hpp
+++ testtemplates/templates.hpp
@@ -3,24 +3,20 @@
 
 #include <stdio.h>
 
-template <class Key, class Info>
+
+template<class Key, class Info>
+struct Node{
+    Key key;
+    Info val;
+    Node* next = nullptr;
+};
+
+template<class Key, class Info>
 class Sequence {
-    struct Node{
-        Key key;
-        Info val;
-        Node* next = nullptr;
-    };
-    Node* head;
-    
+    Node<Key,Info>* head;
 public:
     Sequence();
-//    ~Sequence();
-//    
-//    void empty();
-//    void erase();
-//    
-//    void find(Key);
-//    
+
 //    void remove_key(Key);
 //    void remove_pos(int);
 //    void pop();
@@ -42,7 +38,7 @@ Sequence<Key, Info>::Sequence(){
 
 template <class Key, class Info>
 void Sequence<Key,Info>::print() {
-    Node* tmp = head;
+    Node<Key,Info>* tmp = head;
     while(tmp){
         std::cout << tmp->key << " : " << tmp->val << std::endl;
         tmp = tmp->next;
@@ -53,16 +49,16 @@ void Sequence<Key,Info>::print() {
 template <class Key, class Info>
 void Sequence<Key, Info>::push_back(Key key, Info val){
     if(!head){
-        head = new Node;
+        head = new Node<Key,Info>;
         head->key = key;
         head->val = val;
     }
     else{
-        Node* tmp = head;
+        Node<Key,Info>* tmp = head;
         while(tmp->next){
             tmp = tmp->next;
         }
-        tmp->next = new Node;
+        tmp->next = new Node<Key,Info>;
         tmp->next->key = key;
         tmp->next->val = val;
     }
@@ -71,22 +67,29 @@ void Sequence<Key, Info>::push_back(Key key, Info val){
 template<class Key, class Info>
 void Sequence<Key,Info>::push_front(Key key, Info val){
     if(!head){
-        head = new Node;
+        head = new Node<Key,Info>;
         head->key = key;
         head->val = val;
     }
     else{
-        Node* tmp = head;
-        head = new Node;
+        Node<Key,Info>* tmp = head;
+        head = new Node<Key,Info>;
         head->key = key;
         head->val = val;
         head->next = tmp;
     }
 }
+
+
+
+
+
+
 template<class Key, class Info>
 Sequence<Key,Info> produce(const Sequence<Key,Info> &s1, int start1, int len1,
                            const Sequence<Key,Info> &s2, int start2, int len2,
                            int limit){
     
 }
+
 #endif /* templates_hpp */
